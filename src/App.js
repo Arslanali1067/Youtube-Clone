84371770a9261826facc8ed7ced78f4ae131e805
Arslan <ch.arslan.95@gmail.com>
2022-08-13 20:16:40 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { Menu, Navbar } from "./components/";
+import { darkTheme, lightTheme } from "./utils/Theme";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Main = styled.div`
+  flex: 7;
+  background-color: ${({ theme }) => theme.bg};
+`;
+const Wrapper = styled.div``;
+
+function App() {
+  const [darkMode, setDarkMode] = useState(true);
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+        <Main>
+          <Navbar />
+          <Wrapper>video cards</Wrapper>
+        </Main>
+      </Container>
+    </ThemeProvider>
+  );
+}
+
+export default App;
